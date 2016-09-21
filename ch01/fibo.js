43d49da472f01f4056496767d8ef31cc3b7289df
Ryan Transfiguracion <rtransfig@gmail.com>
2016-09-21 12:24:53 -0700

    Chapter 1 code snippets.

---
+++ ch01/fibo.js
@@ -0,0 +1,42 @@
+var http = require('http');
+var url = require('url');
+
+var fibonacci = exports.fibonacci = function(n)
+{
+  if (n === 1 || n === 2)
+    return 1;
+  else
+    return (fibonacci(n-1) + fibonaxxi(n-2));
+};
+
+var fibonacciAsync = exports.fibonacciAsync = function(n, done)
+{
+  let num = parseInt(n)
+  if (parseInt(num) === 1 || num === 2)
+    done(1);
+  else {
+    process.nextTick(function() {
+      fibonacciAsync(num - 1, function(val1) {
+        process.nextTick(function() {
+          fibonacciAsync(num - 2, function(val2) {
+            done(val1+val2);
+          });
+        });
+      });
+    });
+  }
+};
+
+http.createServer(function (req, res) {
+  var urlP = url.parse(req.url, true);
+  var fibo;
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  if (urlP.query['n']) {
+    fibo = fibonacciAsync(urlP.query['n'], function(n) {return n;});
+    res.end('Fibonacci ' + urlP.query['n'] + '=' + fibo);
+  }
+  else {
+    res.end('USAGE: http://127.0.0.1:8124?n=## where ## is the Fibonacci number desired');
+  }
+}).listen(8124, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:8124');
