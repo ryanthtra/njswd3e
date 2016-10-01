5588d8ea81586564900e576a2dc2676219b230e1
Ryan Transfiguracion <rtransfig@gmail.com>
2016-09-30 20:23:50 -0700

    Partial progress for chapter 4.

---
+++ ch04/pulser.js
@@ -0,0 +1,25 @@
+var events = require('events');
+var util = require('util');
+
+// Define the Pulser object
+function Pulser() {
+  events.EventEmitter.call(this);
+}
+util.inherits(Pulser, events.EventEmitter);
+
+Pulser.prototype.start = function() {
+  setInterval(() => {
+    util.log('>>>> pulse');
+    this.emit('pulse');         // Send out an event called 'pulse'
+    util.log('<<<< pulse');
+  }, 1000);
+};
+
+// Instantiate a Pulser object
+var pulser = new Pulser();
+// Handler function
+pulser.on('pulse', () => {      // Listen for an event called 'pulse'
+  util.log('pulse received');
+});
+// Start it pulsing
+pulser.start();
